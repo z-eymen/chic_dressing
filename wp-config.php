bde273dd7edc4bd1e897017179c384acbe4f32cb
zeliha <z_eymen@hotmail.com>
2024-02-07 16:55:54 +0100

    first commit

---
+++ wp-config.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * La configuration de base de votre installation WordPress.
+ *
+ * Ce fichier contient les réglages de configuration suivants : réglages MySQL,
+ * préfixe de table, clés secrètes, langue utilisée, et ABSPATH.
+ * Vous pouvez en savoir plus à leur sujet en allant sur
+ * {@link https://fr.wordpress.org/support/article/editing-wp-config-php/ Modifier
+ * wp-config.php}. C’est votre hébergeur qui doit vous donner vos
+ * codes MySQL.
+ *
+ * Ce fichier est utilisé par le script de création de wp-config.php pendant
+ * le processus d’installation. Vous n’avez pas à utiliser le site web, vous
+ * pouvez simplement renommer ce fichier en "wp-config.php" et remplir les
+ * valeurs.
+ *
+ * @link https://fr.wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
+/** Nom de la base de données de WordPress. */
+define( 'DB_NAME', 'chic_dressing' );
+
+/** Utilisateur de la base de données MySQL. */
+define( 'DB_USER', 'root' );
+
+/** Mot de passe de la base de données MySQL. */
+define( 'DB_PASSWORD', 'root' );
+
+/** Adresse de l’hébergement MySQL. */
+define( 'DB_HOST', 'localhost' );
+
+/** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** Type de collation de la base de données.
+  * N’y touchez que si vous savez ce que vous faites.
+  */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Clés uniques d’authentification et salage.
+ *
+ * Remplacez les valeurs par défaut par des phrases uniques !
+ * Vous pouvez générer des phrases aléatoires en utilisant
+ * {@link https://api.wordpress.org/secret-key/1.1/salt/ le service de clés secrètes de WordPress.org}.
+ * Vous pouvez modifier ces phrases à n’importe quel moment, afin d’invalider tous les cookies existants.
+ * Cela forcera également tous les utilisateurs à se reconnecter.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         '#mlJJnktt:*B=Qs.nq&>|c$X!QNL$T`CGckpEyby1h34:n{K_ICZ5:jSnYv+6A4W' );
+define( 'SECURE_AUTH_KEY',  'cPr$DnVY~>w+x*XsB_FtwEFI=wW*<g+4SN7h1gBZl<t:IKWKCY-bfs[,o-}5/Ul0' );
+define( 'LOGGED_IN_KEY',    'VIek?j90KN)K4l*_*W!tbB&0nApnq INlK +7w2`zcWKd~hiRSZ%IDWFzrkYPFN5' );
+define( 'NONCE_KEY',        'q7m,z1->&pbRdAy2K1U*=JXSX%__-7dZ>$k^H,LcFzIL@8{/X(PUzCdJX+}|TLiL' );
+define( 'AUTH_SALT',        'T0%IEncqH1`vsFoGo{^`4R^j*w8:nzG$?z9dK/vlz@z<2qV]B>?q`g6E>x7TjJx9' );
+define( 'SECURE_AUTH_SALT', '-3vWI<dKxszoC!gGl.ML(iStHK[=8:(6=LndXq=XJW}!1hrZ7aIa~0$6p$-+g%=#' );
+define( 'LOGGED_IN_SALT',   'nI9Hysnf=N(B-9>z+lhDg~SEvmZ[F.b.tlSli!Cakv=C3x4d`jF@~>#i`0w<|fM^' );
+define( 'NONCE_SALT',       'yNZHs|oym8p[VGZuRX-l9|]`-*8;!YEi54TY;i$|Ro|?35|ZrLri`M$5pG+xP.-=' );
+/**#@-*/
+
+/**
+ * Préfixe de base de données pour les tables de WordPress.
+ *
+ * Vous pouvez installer plusieurs WordPress sur une seule base de données
+ * si vous leur donnez chacune un préfixe unique.
+ * N’utilisez que des chiffres, des lettres non-accentuées, et des caractères soulignés !
+ */
+$table_prefix = 'wp_';
+
+/**
+ * Pour les développeurs et développeuses : le mode déboguage de WordPress.
+ *
+ * En passant la valeur suivante à "true", vous activez l’affichage des
+ * notifications d’erreurs pendant vos essais.
+ * Il est fortement recommandé que les développeurs et développeuses d’extensions et
+ * de thèmes se servent de WP_DEBUG dans leur environnement de
+ * développement.
+ *
+ * Pour plus d’information sur les autres constantes qui peuvent être utilisées
+ * pour le déboguage, rendez-vous sur la documentation.
+ *
+ * @link https://fr.wordpress.org/support/article/debugging-in-wordpress/
+ */
+define('WP_DEBUG', true);
+define( 'WP_DEBUG_LOG', true);
+
+/* C’est tout, ne touchez pas à ce qui suit ! Bonne publication. */
+
+/** Chemin absolu vers le dossier de WordPress. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Réglage des variables de WordPress et de ses fichiers inclus. */
+require_once(ABSPATH . 'wp-settings.php');
---
+++ wp-content/plugins/facebook-for-woocommerce/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'facebookincubator/facebook-for-woocommerce',
+        'pretty_version' => 'dev-release/3.1.9',
+        'version' => 'dev-release/3.1.9',
+        'reference' => '8b129a9ad9cb1347051c82caa8c73da36868f2bc',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'facebookincubator/facebook-for-woocommerce' => array(
+            'pretty_version' => 'dev-release/3.1.9',
+            'version' => 'dev-release/3.1.9',
+            'reference' => '8b129a9ad9cb1347051c82caa8c73da36868f2bc',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'woocommerce/action-scheduler-job-framework' => array(
+            'pretty_version' => '2.0.0',
+            'version' => '2.0.0.0',
+            'reference' => 'b0b21b9cc87e476ba7f8817050b39274ea7d6732',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../woocommerce/action-scheduler-job-framework',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
